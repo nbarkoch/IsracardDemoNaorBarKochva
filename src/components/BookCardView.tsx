7a2b7aaf52247f7941eda781f99cf66e1d91cf44
nbarkoch <naor.barkochva@bankleumi.co.il>
2024-12-19 13:14:22 +0200

    book card view data settled

--- src/components/BookCardView.tsx
+++ src/components/BookCardView.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Book } from "../utils/types";
 
 interface BookCardViewProps {
@@ -7,12 +7,50 @@ interface BookCardViewProps {
   onPress: () => void;
 }
 
-const BookCardView = ({ onPress, book }: BookCardViewProps) => {
+const BookCardView = ({
+  onPress,
+  book: { cover, title, releaseDate },
+}: BookCardViewProps) => {
   return (
-    <TouchableOpacity style={{ padding: 20 }} onPress={onPress}>
-      <Text key={book.index}>{book.title}</Text>
+    <TouchableOpacity style={styles.card} onPress={onPress}>
+      <Image
+        source={{ uri: cover }}
+        style={styles.cover}
+        resizeMode="contain"
+      />
+      <View style={styles.info}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.releaseData}> {releaseDate}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 20,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "grey",
+  },
+  info: {
+    flexDirection: "column",
+    flexShrink: 1,
+    paddingHorizontal: 10,
+    gap: 5,
+  },
+  cover: {
+    width: 100,
+    height: 150,
+  },
+  title: {
+    fontWeight: "900",
+    fontSize: 18,
+  },
+  releaseData: {
+    fontSize: 16,
+    color: "grey",
+  },
+});
 
 export default BookCardView;
